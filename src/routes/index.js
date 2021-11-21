6ed71a32b76d2bdc20efb26386767baf40d0e83f
John Margaronis <john@johnmargaronis.com>
2021-11-21 15:10:11 -0600

    Squashed a couple of bugs.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,13 @@ const DiscordUser = require('../models/DiscordUser');
 const { ensureAuthDashboard } = require('../config/auth');
 
 // Landing page
-router.get('/', ensureAuthDashboard, (req, res) => res.render('welcome'));
+router.get('/', async (req, res) => {
+  // req.session.valid = true;
+  if (req.user) {
+    var discordUser = await DiscordUser.findOne({ user: req.user.id });
+    res.redirect('/dashboard');
+  } else {
+    res.render('welcome', { discordUser });
+  }
+});
 module.exports = router;
